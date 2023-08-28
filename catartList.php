dfd61ae6ecf8eb48ed95669eb21c7bd7cd5152ec
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2023-08-28 17:21:11 +0200

    Add files via upload

---
+++ catartList.php
@@ -0,0 +1,33 @@
+<!-- script affichant liens et résumés des articles de chaque catégorie dans le menu -->
+<h3>
+	<?php $plxShow->lang('CATEGORIES'); ?>
+</h3> 
+<ul class="cat-list unstyled-list">
+	<?php 
+		#configuration filtres
+		$catFilter = array(); 		// lister ici les catégories à afficher par leur identifiant . exemple  array('003', '002', '004');
+		$nbArts ='1';        		// indiquez ici le nombre d'article max à afficher
+		$length ='20';				// indiquez ici le nombre de lettre à afficher
+		
+		#initialisation script 
+		if(count($catFilter)>0) {
+			foreach($catFilter as $cat => $v){
+				$menu[]=$cat;
+			}		
+		}
+		else { // ou parcourez toutes les catégories
+			$menu = $plxShow->plxMotor->aCats;
+		}
+		$more=$plxShow->getLang('L_ARTCHAPO');// recupere la traduction du théme
+		foreach($menu as $id_cat => $ar) {
+			# on stocke l'affichage et on lance la fonction lastArtList()
+			ob_start(); 
+			$plxShow->lastArtList ('<li>#art_chapo('.$length.')<p class="more"><a href="#art_url">'.$more.'</a></p></li>', $nbArts , $id_cat);
+			# on recupere l'affichage stocké dans une variable
+			$listArts = ob_get_clean();// ce que tu veut inserer
+			
+			# reste à insérer la variable ou l'on veut qu'elle réapparaisse.
+			$plxShow->catList('','<li id="menu_#cat_id" class="#cat_status"><a href="#cat_url">#cat_name</a><ul>'. $listArts.'</ul><p><a href="#cat_url" title="Voir tous les articles de la catégorie #cat_name">Voir tous les articles</a></p></li>',
+			$id_cat);
+		} ?>            
+</ul>
